5f258a5e206e1e9bb15c5fff39debbdef32526c2
NikhileshSV <nikhils0696@gmail.com>
2022-06-26 15:17:51 +0530

    production server url added

--- src/static/constants/index.js
+++ src/static/constants/index.js
@@ -1,4 +1,4 @@
-const productionURL = "https://expensestracker-server.herokuapp.com/"; // Production
+const productionURL = "https://expensestracker-server.herokuapp.com"; // Production
 const developmentURL = "http://localhost:5000"; // Development
 
 export const config = process.env.NODE_ENV === "development" ? developmentURL : productionURL;
